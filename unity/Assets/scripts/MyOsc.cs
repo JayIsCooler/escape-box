619fe297de521af4b4940f12ff2a4033a7581546
Étienne Charron <93718161+etennecharron@users.noreply.github.com>
2023-12-05 21:01:08 -0500

    puzzle_base

--- unity/Assets/scripts/MyOsc.cs
+++ unity/Assets/scripts/MyOsc.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
@@ -14,12 +15,25 @@ public class MyOsc : MonoBehaviour
     public extOSC.OSCTransmitter oscTransmitter;
 
     public GameObject sphere;
+    public GameObject blackLight;
+    private bool blackLightEtat;
+    public GameObject objetSecretsContainer;
+    public GameObject lumierePlafond;
 
+    void Start()
+    {
+        oscReceiver.Bind("/encoder", RotationMessageReceived);
+        oscReceiver.Bind("/key", ButtonMessageReceived);
+        oscReceiver.Bind("/light", lightMessageReceived);
+        //scReceiver.Bind("/pot", PotMessageReceived);
+    }
     public static float ScaleValue(float value, float inputMin, float inputMax, float outputMin, float outputMax)
     {
         return Mathf.Clamp(((value - inputMin) / (inputMax - inputMin) * (outputMax - outputMin) + outputMin), outputMin, outputMax);
     }
 
+
+    /********************************CODE POUR LA COMBINAISON ET LE ENCODER***************************************/
     void RotationMessageReceived(OSCMessage oscMessage)
     {
         float value;
@@ -36,86 +50,103 @@ public class MyOsc : MonoBehaviour
             return;
         }
 
-        //Debug.Log(value);
-
-        float Rotation = sphere.transform.eulerAngles.x; ;
-
         if (value < 0)
         {
-            sphere.transform.eulerAngles = new Vector3( Rotation - 10, sphere.transform.rotation.y ,sphere.transform.rotation.z);
-            Debug.Log("tourne à gauche");
+            sphere.transform.Rotate(sphere.transform.rotation.x + 10, sphere.transform.rotation.y ,sphere.transform.rotation.z);
         }
         else if (value > 0)
         {
-            sphere.transform.eulerAngles = new Vector3(Rotation + 10, sphere.transform.rotation.y, sphere.transform.rotation.z);
-            Debug.Log("tourne à Droite");
+            sphere.transform.Rotate(sphere.transform.rotation.x - 10, sphere.transform.rotation.y, sphere.transform.rotation.z);
         }
+  
+    }
+
+
+    /********************************CODE POUR LA BLACKLIGHT ET LE KEY UNIT***************************************/
+
+    private bool blackLightBtnPrete = false;
+ 
+    void ButtonMessageReceived(OSCMessage oscMessage)
+    {
+       int value = oscMessage.Values[0].IntValue;
+
+        if (value == 1 && blackLightEtat == false && blackLightBtnPrete == false)
+        {
+            blackLight.SetActive(true);
+ 
+            blackLightEtat = true;
+        }
+        else if (value == 0 && blackLightEtat == true && blackLightBtnPrete == false)
+        {
+            blackLightBtnPrete = true;
+        }
+        else if (value == 1 && blackLightEtat == true && blackLightBtnPrete == true)
+        {
+            blackLight.SetActive(false);
+            blackLightEtat = false;
+        }
+        else if (value == 0 && blackLightEtat == false && blackLightBtnPrete == true)
+        {
+            blackLightBtnPrete = false;
+        }
+       
+
 
     }
+
+
+
+    /********************************CODE POUR LA LUMIERE QUI SORS DU PLAFOND ET LE LIGHT UNIT***************************************/
+
+
+
+    void lightMessageReceived(OSCMessage oscMessage)
+    {
+
+        float valeurLumiere;
+        if (oscMessage.Values[0].Type == OSCValueType.Int)
+        {
+            valeurLumiere = oscMessage.Values[0].IntValue;
+        }
+        else if (oscMessage.Values[0].Type == OSCValueType.Float)
+        {
+            valeurLumiere = oscMessage.Values[0].FloatValue;
+        }
+        else
+        {
+
+            return;
+        }
+
+
+        float scaledValeurLumiere = ScaleValue(valeurLumiere, 4096,0, 0, 2);
+        lumierePlafond.GetComponent<Light>().intensity = scaledValeurLumiere;
+
+           
+
+    }
+
+    private void Update()
+    {
+        if (blackLight.activeInHierarchy == true && lumierePlafond.GetComponent<Light>().intensity <= 0.5)
+        {
+            objetSecretsContainer.SetActive(true);
+        }
+        else
+        {
+            objetSecretsContainer.SetActive(false);
+        }
+    }
+
+
+
+
+
+
 
 
 
     /**
-    void ButtonMessageReceived(OSCMessage oscMessage)
-    {
-        float value;
-        if (oscMessage.Values[0].Type == OSCValueType.Int)
-        {
-            value = oscMessage.Values[0].IntValue;
-        }
-        else if (oscMessage.Values[0].Type == OSCValueType.Float)
-        {
-            value = oscMessage.Values[0].FloatValue;
-        }
-        else
-        {
-            return;
-        }
-        if (value != autreValeur && value == 0)
-        {
-            body.AddForce(new Vector2(0, saut), ForceMode2D.Impulse);
-            son.SetActive(true);
-            portesOuvertes.SetActive(true);
-            portesFermer.SetActive(false);
-            lightOn = 0;
-        } else if (value != autreValeur && value == 1)
-        {
-             lightOn = 255;
-            son.SetActive(false);
-            portesOuvertes.SetActive(false);
-            portesFermer.SetActive(true);
-        }
-        autreValeur = value;
-  
-       
-
-    } 
-    void lightMessageReceived(OSCMessage oscMessage)
-        {
-    
-            float valeurLumiere;
-            if (oscMessage.Values[0].Type == OSCValueType.Int)
-            {
-            valeurLumiere = oscMessage.Values[0].IntValue;
-            }
-            else if (oscMessage.Values[0].Type == OSCValueType.Float)
-            {
-            valeurLumiere = oscMessage.Values[0].FloatValue;
-            }
-            else
-            {
-        
-                return;
-            }
-
-            // Changer l'échelle de la valeur pour l'appliquer à la rotation :
-            float opacite = ScaleValue(valeurLumiere, 0, 1800, 0 , 1);
-        // Appliquer la rotation au GameObject ciblé :       
-        opacite = 1.3f - opacite;
-        vision.GetComponent<SpriteRenderer>().material.color = new Color(1f, 1f, 1f, opacite);
-
-    }
-
     void PotMessageReceived(OSCMessage oscMessage)
     {
        
@@ -127,55 +158,25 @@ public class MyOsc : MonoBehaviour
 
     **/
 
-    // Start is called before the first frame update
-    void Start()
-    {
- 
-        oscReceiver.Bind("/encoder", RotationMessageReceived);
-        //oscReceiver.Bind("/key", ButtonMessageReceived);
-       //scReceiver.Bind("/light", lightMessageReceived);
-       //scReceiver.Bind("/pot", PotMessageReceived);
-    }
-
     // Update is called once per frame
-    void Update()
-    {
-        /**
-         * 
-         * 
-         * FUNCTION WEIRD QUE J'AI UTILISER LA DERNIERE FOIS POUR LA ROTATION
-         * 
-         * 
-        float Rotation;
-        if (sphere.transform.eulerAngles.x <= 180f)
-        {
-            Rotation = sphere.transform.eulerAngles.x;
-        }
-        else
-        {
-            Rotation = sphere.transform.eulerAngles.x - 360f;
-        }
-        Debug.Log("Rotation avec function "+Rotation);
-        Debug.Log("blblblblblb " + sphere.transform.eulerAngles.x);
-        **/
-    }
 
     float startChrono;
 
     void LateUpdate()
     {
-     // float position = boite.transform.position.x;
-     // position = ScaleValue(position, -7, 7, 0, 255);
+        // float position = boite.transform.position.x;
+        // position = ScaleValue(position, -7, 7, 0, 255);
 
-        
 
+        /**
         if(Time.realtimeSinceStartup - startChrono > 0.050f){
             startChrono = Time.realtimeSinceStartup;
             extOSC.OSCMessage message = new OSCMessage("/pixel");
-     //     message.AddValue(extOSC.OSCValue.Int((int)lightOn));
+      message.AddValue(extOSC.OSCValue.Int((int)lightOn));
             oscTransmitter.Send(message);
         }
   
-
+    **/
     }
+
 }
